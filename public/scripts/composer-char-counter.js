f35d6b3c8ffb359e1aef2dd578c97c299dbe60d6
chanoonna <chanoonna@gmail.com>
2021-01-27 04:26:14 +0000

    nothing changed

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
+
+  // Couldn't find a way to fire up the listener for not releasing a key. Update if has knowledge.
   $('#tweet-text').on('keydown keyup', function() {
+    const $parent = $(this).parent();
+
     if (this.value.length > 140) {
-      $(this).parent().find('.counter').addClass('counter-red');
+      $parent.find('.counter').addClass('counter-red');
+      $parent.find('button').addClass('button-function');
+      $parent.find('button').attr('disabled', true);
     } else {
-      $(this).parent().find('.counter').removeClass('counter-red');
+      $parent.find('.counter').removeClass('counter-red');
+      $parent.find('button').removeClass('button-function');
+      $parent.find('button').removeAttr('disabled');
     }
   });
 });
