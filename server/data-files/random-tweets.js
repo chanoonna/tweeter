951210a1c0d4328b4b16dc0a753698d1910bad4e
chanoonna <chanoonna@gmail.com>
2021-01-28 05:18:45 +0000

    mock tweets for SSE test

---
+++ server/data-files/random-tweets.js
@@ -0,0 +1,92 @@
+const sseTest = [
+  {
+    "user": {
+      "name": "Cpl. Ferro",
+      "avatars": "https://i.imgur.com/z5LNkkB.png",
+      "handle": "@KillAliens"
+    },
+    "content": {
+      "text": "We're in the pipe 5 by 5."
+    },
+    "created_at": 0
+  },
+  {
+    "user": {
+      "name": "Mr. McGuire",
+      "avatars": "https://i.imgur.com/5fUVPRP.png",
+      "handle": "@OilRocks"
+    },
+    "content": {
+      "text": "I just wanna say one word to you, one word. Plastics."
+    },
+    "created_at": 0
+  },
+  {
+    "user": {
+      "name": "Tony",
+      "avatars": "https://i.imgur.com/z5LNkkB.png",
+      "handle": "@IHATEDIAMONDS"
+    },
+    "content": {
+      "text": "Her name was Lola and she was a show girl. I loved her so much and that diamond waring bastard Rico took her away from me!"
+    },
+    "created_at": 0
+  },
+  {
+    "user": {
+      "name": "Rico",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@ILOVEDIAMONDS"
+    },
+    "content": {
+      "text": "@IHATEDIAMONDS Tony, get over it. I told you! She wasn't into you. She loves diamonds just like me!"
+    },
+    "created_at": 0
+  },
+  {
+    "user": {
+      "name": "Lola",
+      "avatars": "https://i.imgur.com/lRUnDgU.png",
+      "handle": "@Ipretty"
+    },
+    "content": {
+      "text": "@IHATEDIAMONDS @IHATEDIAMONDS Both of you stop it! Tony, I am a way out of your league! And Rico, you have a wife!"
+    },
+    "created_at": 0
+  },
+  {
+    "user": {
+      "name": "Cpt. Soolly",
+      "avatars": "https://i.imgur.com/ilT4JDe.png",
+      "handle": "@H3R0"
+    },
+    "content": {
+      "text": "Yes! Birds! ... I mean, don't come to us."
+    },
+    "created_at": 0
+  },
+  {
+    "user": {
+      "name": "Cole Sear",
+      "avatars": "https://i.imgur.com/2WZtOD6.png",
+      "handle": "@BruceIsDead"
+    },
+    "content": {
+      "text": "I see dead people."
+    },
+    "created_at": 0
+  },
+  {
+    "user": {
+      "name": "Sir Alex Ferguson",
+      "avatars": "https://i.imgur.com/5fUVPRP.png",
+      "handle": "@ManUtd"
+    },
+    "content": {
+      "text": "Tweeter is a waste of time."
+    },
+    "created_at": 0
+  }
+];
+
+module.exports = sseTest;
